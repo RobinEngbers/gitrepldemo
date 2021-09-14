a100d73d0eca1e851aff3e286f283a2fb898f122
Robin Engbers <90309164+RobinEngbers@users.noreply.github.com>
2021-09-14 20:50:13 +0000

    stuiterbal

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
-let x = 0;
-let y = 0;
+var x, y, vx, vy;
 
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(500, 500);
+
+  x = 30;
+  y = 200;
+  vx = 5;
+  vy = 5;
 }
 
 function draw() {
-  background("#0055ff");
-  noStroke();
-  ellipse(x, y, 50, 50);
-  
-  x = x+1
-  y = y+1
+    background(255);
+
+  ellipse(x,y,50,50);
+  x = x + vx;
+  y = y + vy;
+
+  ellipse(x,y,50,50);
+  x = x + vx;
+  y = y + vy;
+
+  if (x < 25 || x > 475){
+    vx = vx * -1;
+  }
+
+  if (y < 25 || y > 475){
+    vy = vy * -1;
+  }
 }
