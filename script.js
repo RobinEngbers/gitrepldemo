642ff53c8ed06a8be27a4c911ee69d892fa87a52
Robin Engbers <90309164+RobinEngbers@users.noreply.github.com>
2021-09-15 08:04:32 +0000

    class en constructor toegevoegd

--- script.js
+++ script.js
@@ -1,30 +1,41 @@
-var x, y, vx, vy;
+class Ball {
+  constructor(x, y, w, h, vx, vy) {
+    this.x = 30;
+    this.y = 200;
+    this.w = 50
+    this.h = 50
+    this.vx = 5;
+    this.vy = 5;
+  }
+
+   drawBall(){
+    ellipse(this.x,this.y,this.w,this.h);
+      this.x = this.x + this.vx;
+      this.y = this.y + this.vy;
+
+    if (this.x < 25 || this.x > 475){
+      this.vx = this.vx * -1;
+    }
+
+    if (this.y < 25 || this.y > 475){
+      this.vy = this.vy * -1;
+    }
+   }
+}
+var ball1, ball2, ball3
 
 function setup() {
   createCanvas(500, 500);
 
-  x = 30;
-  y = 200;
-  vx = 5;
-  vy = 5;
+  ball1 = new Ball(30,200,50,50,5,5,"red");
+  ball2 = new Ball(120,210,50,50,3,3,"blue");
+  ball3 = new Ball(200,150,50,50,1,1,"green");
 }
 
 function draw() {
-    background(255);
+  background(0);
 
-  ellipse(x,y,50,50);
-  x = x + vx;
-  y = y + vy;
-
-  ellipse(x,y,50,50);
-  x = x + vx;
-  y = y + vy;
-
-  if (x < 25 || x > 475){
-    vx = vx * -1;
-  }
-
-  if (y < 25 || y > 475){
-    vy = vy * -1;
-  }
+  ball1.drawBall();
+  ball2.drawBall();
+  ball3.drawBall();
 }
